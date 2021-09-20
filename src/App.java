c8a8697b013a6156be49c16e185f87b0b2f7a2ef
Sallai András <andrewteki@gmail.com>
2021-09-20 12:11:41 +0200

    calcArea() tiszta

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Triangle triangle = new Triangle();
-        double area = triangle.calcArea(30, 35);
+        triangle.setBase(30);
+        triangle.setHeight(35);
+        double area = triangle.calcArea();
         System.out.println("Area: " + area);
     }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,5 +1,16 @@
 public class Triangle {
-    public double calcArea(double base, double height) {
+    private double base;
+    private double height;
+
+    public double calcArea() {
         return base * height / 2;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
     }
 }
